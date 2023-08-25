32bbbc3408f86272687f8bb5841b1dd823f6f9a3
FahadSalamatRandhawa <dreamtwister61814@gmail.com>
2023-08-25 14:00:21 +0500

    p8

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,11 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import path from "path";
-import getConfig from 'next/config'
-
 
 export const GET=async(request:NextRequest)=>{
-    const { serverRuntimeConfig } = getConfig()
-    return NextResponse.json({message:'api working',path:path.join(process.cwd(),'signin'),relative:path.join(process.cwd(),'./')},{status:200,headers:{
+    return NextResponse.json({message:'api working',path:path.join(process.cwd(),'signin')},{status:200,headers:{
         'application-type':'json/text'
     }})
 }
