ad5ed039e029ead5ec49e63198df82f623caa8e6
FahadSalamatRandhawa <dreamtwister61814@gmail.com>
2023-08-25 15:35:07 +0500

    path11

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from "next/server";
 import path from "path";
 
 export const GET=async(request:NextRequest)=>{
-    const private_key=readFileSync(path.join(process.cwd(),'src/certs','private.pem'))
+    const private_key=readFileSync(path.join(process.cwd(),'.next/server/certs','private.pem'))
     return NextResponse.json({message:'api working',private_key},{status:200,headers:{
         'application-type':'json/text'
     }})
